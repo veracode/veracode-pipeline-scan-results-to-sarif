24c78bfdbd3a13386e356891d134694b68f0515d
Julian Totzek-Hallhuber <j.totzek@gmail.com>
2024-02-12 21:59:15 +0100

    adding await

--- src/index.ts
+++ src/index.ts
@@ -68,7 +68,7 @@ async function uploadSARIF(outputFilename:any, opt:any) {
         }
     }
 
-    const base64Data = createGzipBase64(outputFilename)
+    const base64Data = await createGzipBase64(outputFilename)
     console.log('Base64 data: '+base64Data);
     request('POST /repos/'+opt.repo_owner+'/'+opt.repo_name+'/code-scanning/sarifs', {
         headers: {
