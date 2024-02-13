11fc2f3f9f7e1a14fd5d0128640ef6cb1fb78d7c
Julian Totzek-Hallhuber <j.totzek@gmail.com>
2024-02-13 05:39:00 +0100

    removing owner and repo

--- dist/index.js
+++ dist/index.js
@@ -29333,8 +29333,6 @@ function uploadSARIF(outputFilename, opt) {
             headers: {
                 authorization: opt.githubToken
             },
-            owner: opt.repo_owner,
-            repo: opt.repo_name,
             ref: opt.ref,
             commit_sha: opt.commitSHA,
             sarif: base64Data
