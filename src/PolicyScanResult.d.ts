4a682e691b71aebfbdb36d9e2f4c98441fd6e00b
Shailesh Hande <shande@veracode.com>
2024-02-23 17:38:27 +0530

    added model class for policy scan flaws report

---
+++ src/PolicyScanResult.d.ts
@@ -0,0 +1,68 @@
+export interface PolicyScanResult {
+    _embedded: {
+        findings: Finding[];
+    };
+}
+
+export interface CWE {
+    id: number;
+    name: string;
+    href: string;
+}
+
+export interface FindingCategory {
+    id: number;
+    name: string;
+    href: string;
+}
+
+export interface FindingDetails {
+    severity: number;
+    cwe: CWE;
+    file_path: string;
+    file_name: string;
+    module: string;
+    relative_location: number;
+    finding_category: FindingCategory;
+    procedure: string;
+    exploitability: number;
+    attack_vector: string;
+    file_line_number: number;
+}
+
+export interface FindingStatus {
+    first_found_date: string;
+    status: string;
+    resolution: string;
+    mitigation_review_status: string;
+    new: boolean;
+    resolution_status: string;
+    last_seen_date: string;
+}
+
+export interface Finding {
+    issue_id: number;
+    scan_type: string;
+    description: string;
+    count: number;
+    context_type: string;
+    context_guid: string;
+    violates_policy: boolean;
+    finding_status: FindingStatus;
+    finding_details: FindingDetails;
+    build_id: number;
+}
+
+export interface Links {
+    self: {
+        href: string;
+        templated?: boolean;
+    };
+    application: {
+        href: string;
+    };
+    sca: {
+        href: string;
+        templated?: boolean;
+    };
+}
